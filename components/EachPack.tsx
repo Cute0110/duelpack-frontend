9da13c5664799e24c015d86676bb28933d623cae
Cute0110 <gabefc1104@gmail.com>
2025-03-17 16:22:28 -0700

    Addicted pack added

--- components/EachPack.tsx
+++ components/EachPack.tsx
@@ -9,7 +9,7 @@ const EachPack = ({ data, onClickViewItem }: any) => {
             <div className="absolute top-4 right-4 bg-[#1d2125] p-2 rounded-xl opacity-0 group-hover:opacity-100" onClick={() => onClickViewItem(data)}>
                 <ViewSVG className="h-4 w-auto text-gray-300 z-10" />
             </div>
-            <img src={`./images/packs/${data.name}/${data.imageUrl}`} className="rounded-xl" />
+            <img src={`./images/packs/${data.imageUrl}`} className="rounded-xl" />
             <p className="text-center mt-4 font-semibold text-lg">${data.price.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
         </div>
     );
