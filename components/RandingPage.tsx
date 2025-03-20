f408f5315fafd043114105a205f51e4133adb7ad
Cute0110 <gabefc1104@gmail.com>
2025-03-20 11:37:32 -0700

    error fixed

--- components/RandingPage.tsx
+++ components/RandingPage.tsx
@@ -81,7 +81,7 @@ const RandingPage = ({ packsData, forgeData }: any) => {
     setSelectedPackData(data);
     setIsPackItemsModalOpen(true);
     try {
-      const response = await axiosInstance.post('/api/pack_items_list', eot({ start: 0, length: 0, search: 0, order: "order", dir: "asc", packID: data.id }));
+      const response = await axiosInstance.post('/api/pack_items_list', eot({ start: 0, length: 0, search: 0, order: "rarity", dir: "asc", packID: data.id }));
       const res = dot(response.data);
       if (res.status == 1) {
         setItemsData(res.data);
