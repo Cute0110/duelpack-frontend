69c318d0a7fd6f60d0bd986652e25d150ef0cf7b
Cute0110 <gabefc1104@gmail.com>
2025-03-31 05:15:37 -0700

    pack Images changed

--- components/EachPack.tsx
+++ components/EachPack.tsx
@@ -14,7 +14,7 @@ const EachPack = ({ data, onClickViewItem }: any) => {
     }
     return (
         <div className="rounded-xl cursor-pointer group relative">
-            <div className="absolute top-4 right-4 bg-[#1d2125] p-2 rounded-xl" onClick={() => onClickViewItem(data)}>
+            <div className="absolute top-4 right-4 bg-[#1d2125] p-2 rounded-xl opacity-0 group-hover:opacity-100" onClick={() => onClickViewItem(data)}>
                 <ViewSVG className="h-4 w-auto text-gray-300 z-10" />
             </div>
             <img src={`./images/packs/${data.imageUrl}`} className="rounded-xl" onClick={() => onClickPack(data.id)}/>
